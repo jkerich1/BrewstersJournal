856924ffeedfb70738c739ca977965bafe332816
jkerich1 <49698549+jkerich1@users.noreply.github.com>
2021-05-12 14:10:45 -0500

    Final Recipe Bootstrap
    Added Shading and cleaned up Recipe Page with bootstrap

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -66,23 +66,26 @@ class Recipe extends Component {
         const newState = this.state;
         newState.Boiling = value
         this.setState(newState)
-        this.sendData(newState)    }
+        this.sendData(newState)
+    }
 
     handleOnChangeMash(value) {
         const newState = this.state;
         newState.Mashing = value
         this.setState(newState)
-        this.sendData(newState)    }
+        this.sendData(newState)
+    }
 
     handleOnChangeNotes(value) {
         const newState = this.state;
         newState.Notes = value
         this.setState(newState)
-        this.sendData(newState)    }
+        this.sendData(newState)
+    }
 
     render() {
         return (
-            <div>
+            <div className="shading">
                 <h3>Recipe Upload/Download</h3>
                 <p>Upload Recipe JSON</p>
                 <input type="file" class="form-control-file" id="selectFiles" /><br />
@@ -93,10 +96,10 @@ class Recipe extends Component {
                     Import Recipe File
                 </Button>
                 <br />
-                <textarea class="form-control" rows = "8"  id="result"
+                <textarea class="col-sm-10" rows="8" id="result"
                     value={JSON.stringify(this.state, null, 2)}>
 
-                </textarea>
+                </textarea> <br></br>
                 <a
                     href={`data:text/json;charset=utf-8,${encodeURIComponent(
                         JSON.stringify(this.state, null, 2)
@@ -105,44 +108,49 @@ class Recipe extends Component {
                 >
                     {`Download Json`}
                 </a> <br></br>
-            
-                <label htmlFor="full">Edit Full Json: </label>
-                <input type="text" class="form-control" id="full"
-                    onChange={event => this.handleOnChange(event.target.value)}
-                    value={JSON.stringify(this.state, null, 2)}>
+                <form>
+                    <label htmlFor="full">Edit Full Json: </label>
+                    <input type="text" class="form-control" id="full"
+                        onChange={event => this.handleOnChange(event.target.value)}
+                        value={JSON.stringify(this.state, null, 2)}>
 
-                </input> <br></br>
-                <div>
-                <label htmlFor="name">Name: </label>
-                <input type="text" class="form-control-sm" id="name"
-                    onChange={event => this.handleOnChangeName(event.target.value)}
-                    value={this.state.Name}>
+                    </input> <br></br> <br></br>
+                    <div class="col-sm-10">
+                        <label htmlFor="name">Name: </label>
+                        <input type="text" class="form-control-sm" id="name"
+                            onChange={event => this.handleOnChangeName(event.target.value)}
+                            value={this.state.Name}>
 
-                </input> <br></br>
-                <label htmlFor="category">Category: </label>
-                <input type="text" class="form-control-sm" id="category"
-                    onChange={event => this.handleOnChangeCat(event.target.value)}
-                    value={this.state.Category}>
+                        </input>
+                        <label htmlFor="category">Category: </label>
+                        <input type="text" class="form-control-sm" id="category"
+                            onChange={event => this.handleOnChangeCat(event.target.value)}
+                            value={this.state.Category}>
 
-                </input> 
-                </div>
-                <label htmlFor="boiling">Boiling: </label>
-                <input type="text" class="form-control" id="boiling"
-                    onChange={event => this.handleOnChangeBoil(event.target.value)}
-                    value={this.state.Boiling}>
+                        </input>
+                    </div>
+                    <div class="col-sm-10">
+                        <label htmlFor="boiling">Boiling: </label>
+                        <input type="text" class="form-control" id="boiling"
+                            onChange={event => this.handleOnChangeBoil(event.target.value)}
+                            value={this.state.Boiling}>
 
-                </input> <br></br>
-                <label htmlFor="mashing">Mashing: </label>
-                <input type="text" class="form-control" id="mashing"
-                    onChange={event => this.handleOnChangeMash(event.target.value)}
-                    value={this.state.Mashing}>
+                        </input> </div>
+                    <div class="col-sm-10">
+                        <label htmlFor="mashing">Mashing: </label>
+                        <input type="text" class="form-control" id="mashing"
+                            onChange={event => this.handleOnChangeMash(event.target.value)}
+                            value={this.state.Mashing}>
 
-                </input> <br></br>
-                <label htmlFor="notes">Notes: </label>
-                <input type="text" class="form-control" id="notes"
-                    onChange={event => this.handleOnChangeNotes(event.target.value)}
-                    value={this.state.Notes}>
-                </input>
+                        </input></div>
+                    <div class="col-sm-10">
+                        <label htmlFor="notes">Notes: </label>
+                        <input type="text" class="form-control" id="notes"
+                            onChange={event => this.handleOnChangeNotes(event.target.value)}
+                            value={this.state.Notes}>
+                        </input>
+                    </div> <br></br>
+                </form>
             </div>
         )
     }
